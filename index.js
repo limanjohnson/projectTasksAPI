c247957fb2a25b58652a8f02d1754fe61adad9cd
Liam <liam@simplifybiz.com>
2025-05-20 17:18:45 -0600

    getting started

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app
+    .use(cors())
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use('/', require('./routes'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
+
+const db = require('./models');
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to the database!');
+    })
+    .catch((err) => {
+        console.log('Cannot connect to the database!', err);
+        process.exit();
+    });
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
